3cd9e8f1a1353cad53b8e89a970f28558d284cc7
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-10-30 17:27:16 +0530

    Created Todo List with Hooks, Filter and Map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import './App.css';
+import UseStateArray from './components/UseStateArray';
 import UseStateObject from './components/UseStateObject';
 
 const App = () => {
   return (
     <div>
-      <UseStateObject/>
+      <UseStateArray/>
     </div>
   );
 }
--- src/components/UseStateArray.js
+++ src/components/UseStateArray.js
@@ -19,15 +19,28 @@ const UseStateArray = () => {
     const clearArray = () => {
         setmyArray([]);
     }
+
+    const removeElem = (id) => {
+        const myNewArray = myArray.filter((curElem) => {
+            return curElem.id !== id;
+        })
+
+        setmyArray(myNewArray);
+    }
     return (
         <>
-            {
-                myArray.map((curEle) => {
-                    return <h1 className="h1Style" key={curEle.id}> Name: {curEle.myName}
-                     , Age: {curEle.age}</h1>
-                })
-            }
-            <button className="btn" onClick ={clearArray} >Clear </button>
+        {
+            myArray.map((curEle) => {
+                return (
+                    <h1 className="h1Style" key={curEle.id}> 
+                    Name: {curEle.myName}
+                    ,Age: {curEle.age}
+                    <button className="btnInner"
+                            onClick={() =>removeElem(curEle.id)}>Remove</button></h1>
+                ); 
+            })
+        }
+        <button className="btn" onClick ={clearArray} >Clear </button>
         </>
     );
 }
