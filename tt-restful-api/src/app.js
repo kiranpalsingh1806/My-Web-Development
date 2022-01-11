e46a9f854f18efe61d9fd4adb95f6d0c4d18ccdd
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-11 08:47:20 +0530

    Change Promise Code To Async Await

--- tt-restful-api/src/app.js
+++ tt-restful-api/src/app.js
@@ -8,16 +8,26 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/students", function (req, res) {
-    const user = new Student(req.body);
-    console.log(user);
+// app.post("/students", function (req, res) {
+//     const user = new Student(req.body);
+//     console.log(user);
 
-    user.save().then(() => {
-        res.status(201).send(user);
-    }).catch((e) => {
+//     user.save().then(() => {
+//         res.status(201).send(user);
+//     }).catch((e) => {
+//         res.status(400).send(e);
+//     });
+// });
+
+app.post("/students", async (req, res) => {
+    try {
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+    } catch (e) {
         res.status(400).send(e);
-    });
-})
+    }
+});
 
 app.listen(port, function (req, res) {
     console.log(`Server is running on port at ${port}.`);
