9df07d035977340a30dbfb97c3abec86a5b8fa3b
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-12 10:34:15 +0530

    Add Chartjs Starter Code

---
+++ tm-chartjs-react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Chart2 from "./components/Chart.js"
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+      <Chart2 />
+    </div>
+
+  );
+}
+
+export default App;
