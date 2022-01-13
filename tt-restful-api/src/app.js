fe4584949a21174c571a915c8bffb3c394906cc5
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-13 15:57:38 +0530

    Add DELETE, PUT, PATCH, GET to StudentAPI

--- tt-restful-api/src/app.js
+++ tt-restful-api/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
 
 require("./db/conn");
@@ -29,6 +30,56 @@ app.post("/students", async (req, res) => {
     }
 });
 
+
+app.get("/students", async (req, res) => {
+    try {
+        const studentsData = await Student.find();
+        res.send(studentsData);
+    } catch (e) {
+        res.send(e);
+    }
+});
+
+app.get("/students/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const studentData = await Student.findById(_id);
+        res.send(studentData);
+
+        if (!studentData) {
+            return res.status(404).send();
+        } else {
+            res.send(studentData);
+        }
+    } catch (e) {
+        res.send(e);
+    }
+});
+
+app.patch("/students/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updateStudent = await Student.findByIdAndUpdate(id, req.body);
+        res.send(updateStudent);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+})
+
+app.delete("/students/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deleteStudent = await Student.findByIdAndDelete(id);
+        if (!req.params.id) {
+            return res.status(400).send();
+        }
+        res.send(deleteStudent);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
 app.listen(port, function (req, res) {
     console.log(`Server is running on port at ${port}.`);
-})
+});
+
