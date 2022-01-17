443033c860056e738398fba1d08e47d4d712675d
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-17 19:31:06 +0530

    Add data to firestore using form

--- pt-blog-firebase/src/App.js
+++ pt-blog-firebase/src/App.js
@@ -23,17 +23,22 @@ function App() {
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/createpost">Create Post</Link>
         {
-          !isAuth ? <Link to="/login">Login</Link> : <button onClick={signUserOut}>Log Out</button>
-        }
+          !isAuth ? (
+            <Link to="/login">Login</Link>
+          ) : (
+            <>
+              <Link to="/createpost">Create Post</Link>
+              <button onClick={signUserOut}>Log Out</button>
+            </>
+          )}
       </nav>
       <Routes>
         <Route path="/" element={<Home />} ></Route>
         <Route path="/login" element={<Login setIsAuth={setIsAuth} s />}></Route>
-        <Route path="/createPost" element={<CreatePost />}></Route>
+        <Route path="/createPost" element={<CreatePost isAuth={isAuth} />}></Route>
       </Routes>
-    </Router>
+    </Router >
   );
 }
 
--- pt-blog-firebase/src/pages/CreatePost.js
+++ pt-blog-firebase/src/pages/CreatePost.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase-config";
+import { useNavigate } from 'react-router-dom';
 
-const Createpost = () => {
+
+const Createpost = ({ isAuth }) => {
+    const [title, setTitle] = useState("");
+    const [postText, setPostText] = useState("");
+
+    const postsCollectionRef = collection(db, "posts");
+
+    let navigate = useNavigate();
+
+    const createPost = async () => {
+        await addDoc(postsCollectionRef, {
+            title, postText, author: { name: auth.currentUser.displayName, id: auth.currentUser.uid }
+        });
+        navigate("/");
+    }
+
+    useEffect(() => {
+        if (!isAuth) {
+            navigate("/login")
+        }
+    }, []);
+
     return (
         <div className='createPostPage'>
             <div className='cpContainer'>
@@ -9,16 +33,21 @@ const Createpost = () => {
                     <label>
                         Title
                     </label>
-                    <input placeholder='Title...'>
+                    <input
+                        placeholder='Title...'
+                        onChange={(event) => { setTitle(event.target.value) }}>
                     </input>
                 </div>
                 <div className='inputGp'>
                     <label>
                         Post
                     </label>
-                    <textarea placeholder="Post..." />
+                    <textarea placeholder="Post..."
+                        onChange={(event) => {
+                            setPostText(event.target.value)
+                        }} />
                 </div>
-                <button>
+                <button onClick={createPost}>
                     Submit Post
                 </button>
             </div>
